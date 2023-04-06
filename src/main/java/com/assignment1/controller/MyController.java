f89b34bbb30dafcde186f8589453d3c074f2009c
Divya59-Patil <divyapatil200259@gmail.com>
2023-04-06 10:21:21 +0530

    spring rest

---
+++ src/main/java/com/assignment1/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.assignment1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@GetMapping("/")
+	public String helloWorld() {
+	  return "Hello World";
+	}
+}
